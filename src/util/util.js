b80927eca081e8f9a4c46c351b207ac545536ed1
unknown <atikrantnegi@gmail.com>
2020-02-21 18:14:35 +0530

    Added wiki search

--- src/util/util.js
+++ src/util/util.js
@@ -1,3 +1,5 @@
+const urllib = require('urllib');
+
 const Util = class {
    static fetchAvatar(user, size = 2048) {
       let URL = 'https://cdn.discordapp.com/avatars/';
@@ -10,6 +12,28 @@ const Util = class {
       URL += guild.id+'/';
       URL += guild.icon+`.jpg?size=${size}`;
       return URL;
+   }
+   static searchWiki(name) {
+      name = name.replace(/[ ]+/g, '_');
+      return urllib.request(`https://en.wikipedia.org/wiki/${name}`).then(res => {
+         const data = {};
+         const page = res.data.toString();
+         const bodyContent = page.match(/<[ ]*div[ ]+class[ ]*=[ ]*('|")mw-parser-output('|")(.*?)<noscript/gims)[0];
+   
+         let firstPara = bodyContent.match(/<p>(.*?)<\/p>/gims)[0];
+         firstPara = firstPara.replace(/<[ ]*a[ ]+href[ ]*=[ ]*('|")#cite_note-(.+?)('|")>(.*?)(?=>)/gims, '');
+         firstPara = firstPara.replace(/<(.*?)(?=>)/gi, '').replace(/>/g, '');
+         data.firstPara = firstPara;
+
+         let imageURL = bodyContent.match(/<[ ]*img[ ]+(.*)src[ ]*=[ ]*('|")(.*?)('|")(?=>)/gi)[0].match(/('|")(.*?)('|")/gi)[1];
+         imageURL = 'https:'+imageURL.replace(/("|')/g, '');
+         data.imageURL = imageURL;
+
+         data.link = `https://en.wikipedia.org/wiki/${name}`;
+         return data;
+      }).catch(err => {
+         throw err;
+      });
    }
 };
 
